33fa41357e16b5603abee4b300b9a11ea5ef35b8
Srilatha <ysrilatha0308@gmail.com>
2024-05-13 22:33:00 +0200

    First commit

---
+++ ASP.NET-core-web-app-MVC-Proj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_core_web_app_MVC_Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
